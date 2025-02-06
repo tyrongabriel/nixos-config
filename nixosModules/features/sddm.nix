88594de1d37e54312357394665655c02ebc1ff3c
Tyron Gabriel <51530686+tyrongabriel@users.noreply.github.com>
2025-02-06 15:43:16 +0100

    Removed redundant input

---
+++ nixosModules/features/sddm.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.myNixOS.sddm;
+in
+{
+  option.myNixOS.sddm = {
+    enable = lib.mkEnableOption "Enable SDDM";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # https://github.com/catppuccin/sddm
+    environment.systemPackages = [
+      (pkgs.catppuccin-sddm.override {
+        flavor = "mocha";
+        font = "JetBrainsMono Nerd Font Mono";
+        fontSize = "9";
+        background = "${./../../wallpapers/waterfall.png}";
+        loginBackground = true;
+      })
+    ];
+
+    services.xserver = {
+      enable = true;
+      displayManager = {
+        sddm = {
+          enable = lib.mkDefault true;
+          theme = "catppuccin-mocha";
+          package = pkgs.kdePackages.sddm;
+        };
+      };
+    };
+  };
+}
