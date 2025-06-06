ad16714f3dd5e62d570ee3c97780ad87fffae953
tyrongabriel <51530686+tyrongabriel@users.noreply.github.com>
2025-06-06 19:44:56 +0200

    Resolved: added tailscale dns backups

--- nixosModules/features/tailscale.nix
+++ nixosModules/features/tailscale.nix
@@ -21,11 +21,21 @@ in
     };
     services.tailscale.enable = true;
     # Tailscale had my dns broken sometimes... Adding backups
+    # https://github.com/tailscale/tailscale/issues/4254
     networking.nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
-      "1.1.1.1"
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
     ];
+    services.resolved = {
+      enable = true;
+      dnssec = "true";
+      domains = [ "~." ];
+      fallbackDns = [
+        "1.1.1.1#one.one.one.one"
+        "1.0.0.1#one.one.one.one"
+      ];
+      dnsovertls = "true";
+    };
 
     # If my tailnet uses routing features etc. need to configure
     #services.tailscale.useRoutingFeatures = "both" | "server" | "client"
