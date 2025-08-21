d9c05230796a180262803dd9a055f3424fe0893b
tyrongabriel <51530686+tyrongabriel@users.noreply.github.com>
2025-08-21 19:12:26 +0200

    Added nix shell for new pc setup

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  ...
+}:
+pkgs.mkShell {
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+
+  packages = with pkgs; [
+    nh
+    python312Packages.mkdocs-material
+    deploy-rs
+
+    statix
+    deadnix
+    alejandra
+    home-manager
+    git
+    sops
+    ssh-to-age
+    gnupg
+    age
+  ];
+}
