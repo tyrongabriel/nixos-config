56575481f6c70ddd66c9972d7ed81d85a59aef6a
tyrongabriel <51530686+tyrongabriel@users.noreply.github.com>
2025-08-22 01:14:42 +0200

    Updated the amd gpu stuff

--- hosts/laptop-yoga/system/graphics.nix
+++ hosts/laptop-yoga/system/graphics.nix
@@ -6,6 +6,10 @@
   # Load the amdgpu kernel driver early in the boot process.
   # This is good practice for both X11 and Wayland.
   boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.kernelModules = [ "kvm-amd" ];
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = true;
 
   # Enable 3D acceleration with Mesa and its support for VA-API.
   # This is required for hardware video decoding.
@@ -13,9 +17,9 @@
     enable = true;
     driSupport32Bit = true; # Necessary for 32-bit applications
     extraPackages = with pkgs; [
-      mesa.drivers
-      mesa-va-drivers
-      mesa-vdpau-drivers
+      mesa
+      #mesa-va-drivers
+      #mesa-vdpau-drivers
       # For Vulkan support, which is often used by games and some apps.
       # The open-source RADV Vulkan driver is part of mesa.drivers
       vulkan-loader
